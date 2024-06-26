3c3a1e1e936b7e26995eefe9055be5483e0637db
Piyush Gupta <piyushgupta21506@gmail.com>
2024-06-26 14:36:59 +0530

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/Store.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
+      <Analytics />
       <App />
     </BrowserRouter>
   </Provider>
