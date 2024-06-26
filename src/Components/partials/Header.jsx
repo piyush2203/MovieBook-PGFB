261e7704b597ad01ce5c444fcd08f01c96d67db4
Piyush Gupta <piyushgupta21506@gmail.com>
2024-06-26 11:37:54 +0530

    improved header

--- src/Components/partials/Header.jsx
+++ src/Components/partials/Header.jsx
@@ -15,9 +15,9 @@ const Header = ({ data }) => {
         backgroundPosition: "top 10%",
         objectPosition: "center down"
       }}
-      className="w-full h-[50vh] border-y-2 border-white flex flex-col justify-end p-[3%] "
+      className="w-full h-[50vh] relative border-y-2 border-white flex flex-col  p-[3%] "
     >
-      <div className="h-[70%] overflow-y-auto w-full"></div>
+      {/* <div className="h-[70%] overflow-y-auto w-full"></div>
       <h1 className="text-[300%] w-[70%] h-[10vh] font-black text-white">
         {data.original_title || data.title || data.name || data.original_name}
       </h1>
@@ -30,7 +30,36 @@ const Header = ({ data }) => {
       </p>
       <p className="text-white mt-[1.5%] flex gap-3 items-center w-[30%]" ><IoMdAlbums className="text-yellow-400"/> {data.media_type.toUpperCase()}</p>
 
-      <Link to={`/${data.media_type}/details/${data.id}/trailer`} className="bg-[#6556CD] p-[1vh] w-[8vw]  mt-4 rounded">Watch Trailer</Link>
+      <Link to={`/${data.media_type}/details/${data.id}/trailer`} className="bg-[#6556CD] p-[1vh] w-[8vw]  mt-4 rounded">Watch Trailer</Link> */}
+      <div className="w-full h-[20%] "></div>
+      <div className="h-[20%] mb-[1%] overflow-y-auto">
+        <h1 className="text-[300%] w-[70vw]  font-black text-white">
+          {data.original_title || data.title || data.name || data.original_name}
+        </h1>
+      </div>
+      <div className="h-[18%] overflow-y-auto">
+        <p className="w-[85%] text-white ">{data.overview.slice(0,160)}...<Link className="text-zinc-500" to={`/${data.media_type}/details/${data.id}`} >more</ Link >
+        </p>
+      </div>
+      <div className="h-[5%] ">
+        <p className="text-white  h-[5vh] flex gap-3 items-center">
+          <IoMdMegaphone  className="text-yellow-400"/>{data.release_date || "No Info"}
+          
+        </p>
+      </div>
+      <div className="h-[10%] mt-[1.7%]">
+        <p className="text-white  flex gap-3  items-center w-[30%]" ><IoMdAlbums className="text-yellow-400"/> {data.media_type.toUpperCase()}</p>
+      </div>
+
+      <div className="mt-[2%] bottom-[10%]">
+        <Link to={`/${data.media_type}/details/${data.id}/trailer`} className="bg-[#6556CD] p-[1vh] w-[8vw]   rounded">Watch Trailer</Link> 
+      </div>
+
+
+
+      {/* <div className="h-[70%] overflow-y-auto w-full">
+        
+      </div> */}
 
     </div>
   );
